06785a9cd164845d0425fa72238578dbf2d6ca96
saremisadra <sadra@nematifamilyfundation.onmicrosoft.com>
2024-08-08 11:44:33 +0330

    Update __init__.py

--- indoxJudge/piplines/__init__.py
+++ indoxJudge/piplines/__init__.py
@@ -2,6 +2,6 @@ from .llmEvaluator.llm_evaluator import LlmEvaluation
 from .llmEvaluator.graph.llm_comparison import LLMComparison
 from .llmEvaluator.graph.metrics_visualizer import MetricsVisualizer
 from .ragEvaluator.rag_evaluator import RagEvaluator
-from .safetyEvaluator.safety_evaluator import SafetyModel
+from .safetyEvaluator.safety_evaluator import SafetyEvaluator
 
 
