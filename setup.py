8df9ad69841c4c014dec56892c907c701496cb9c
unknown <ashkan.eskandari.dev@gmail.com>
2024-09-07 20:00:24 +0330

    versioning

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='indoxJudge',
-    version='0.0.2',
+    version='0.0.3',
     license='MIT',
     packages=find_packages(),
     package_data={
