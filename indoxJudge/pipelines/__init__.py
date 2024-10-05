36c3f05986842f87e19b47826a4d66667a455285
sadra saremi <sadra@nematifamilyfundation.onmicrosoft.com>
2024-10-05 22:40:50 +0330

    Update __init__.py

--- indoxJudge/pipelines/__init__.py
+++ indoxJudge/pipelines/__init__.py
@@ -3,3 +3,5 @@ from .ragEvaluator.rag_evaluator import RagEvaluator
 from .safetyEvaluator.safety_evaluator import SafetyEvaluator
 from .customEvaluator.custom_evaluator import CustomEvaluator
 from .evaluationAnalyzer.evaluation_analyzer import EvaluationAnalyzer
+from .discriminative import DiscriminativeToxicityEvaluator
+
